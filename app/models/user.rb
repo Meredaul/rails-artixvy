4b4ba8582dc7966101d4844da629573f2cccfd6d
Alvaro Lobato <alobato678@gmail.com>
2017-08-22 11:11:40 +0100

    added models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :artworks
+  has_many :purchases
+  has_many :bought_artworks, through: :purchases, source: :artworks
+
 end
