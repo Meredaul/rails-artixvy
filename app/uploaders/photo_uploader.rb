1b44397bfb468e2fa0372d881979cd56fc1ce5ff
laury meredith <meredith.laury@gmail.com>
2017-08-23 14:41:11 +0100

    could trans

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,4 +4,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
+  version :card_picture do
+    cloudinary_transformation width: 150, height: 220, crop: :fit
+  end
+
+  version :background do
+    cloudinary_transformation width: 800, height: 300, crop: :fit
+  end
+
 end
