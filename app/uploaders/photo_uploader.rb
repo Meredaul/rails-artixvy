681348d04e1381889700412c97d134cefbfba5bb
laury meredith <meredith.laury@gmail.com>
2017-08-23 14:43:56 +0100

    more changes

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,6 +4,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
+  process eager: true
+  process convert: 'jpg'
+
   version :card_picture do
     cloudinary_transformation width: 150, height: 220, crop: :fit
   end
