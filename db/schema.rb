68ebf93cf3342fe561719410b5ad3cf2ce748470
laury meredith <meredith.laury@gmail.com>
2017-08-24 20:40:32 +0100

    autocompletion ok on local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823145620) do
+ActiveRecord::Schema.define(version: 20170824190209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20170823145620) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "image_url"
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "country"
     t.index ["user_id"], name: "index_artworks_on_user_id", using: :btree
   end
 
